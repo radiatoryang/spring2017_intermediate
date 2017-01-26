eb7d3126035b0719de87ccca3f86d8c4ab6c5cd1
Robert Yang <yang.robert.w@gmail.com>
2017-01-26 16:39:23 -0500

    week01_intro: self portrait demo, HelloWorld, TapGame code

---
+++ week01_intro/Assets/scripts/HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour {
+
+	Text myText; // variable for a shortcut to my Text component
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World"); // prints to the Unity console "Hello World"
+		GetComponent<Text>().text = "Bonjour Monde!";
+
+		// cache the reference
+		myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myText.text = Time.time.ToString("F0"); // print Time.time without the fraction
+
+		// "floor" function always rounds down
+		float timeInMinutes = Mathf.Floor(Time.time / 60f); // calculate time in minutes, and print
+		// "\n" goes to the next line, like pressing "enter"
+		myText.text += "\n\tMINUTES: " + timeInMinutes.ToString("F0");
+	}
+}
---
+++ week01_intro/Assets/scripts/TapGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapGame : MonoBehaviour {
+
+	Text myText;
+	int tapCountScore = 0; // remembers how many times we pressed space
+
+	void Start () {
+		myText = GetComponent<Text>();
+	}
+
+	void Update () {
+		myText.text = "PRESS SPACE TO WIN!";
+
+		// add 1 every time player presses space
+		if ( Input.GetKeyDown(KeyCode.Space) ) {
+			// all 3 of these lines do the same thing, just in slightly different ways
+		//	tapCountScore++;
+			tapCountScore += 1;
+		//	tapCountScore = tapCountScore + 1;
+		}
+
+		myText.text += "\n SCORE: " + tapCountScore.ToString();
+	}
+}
