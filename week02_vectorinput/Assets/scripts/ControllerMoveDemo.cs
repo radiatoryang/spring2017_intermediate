1aea47e6ecbd4982903400b49c81750881f56f96
Robert Yang <yang.robert.w@gmail.com>
2017-02-02 17:18:08 -0500

    week02_vectorInput : added sample character controller, see TransformMoveDemo and ControllerMoveDemo

---
+++ week02_vectorinput/Assets/scripts/ControllerMoveDemo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMoveDemo : MonoBehaviour {
+
+	CharacterController ctrl;
+
+	void Start () {
+		ctrl = GetComponent<CharacterController>(); // save reference to our component
+	}
+
+	void Update () {
+		// 1. grab input from input devices
+		float horizontal = Input.GetAxis("Horizontal"); // left/right movement
+		float vertical = Input.GetAxis("Vertical"); // up/down movement
+
+		// 2. plug your values into the character controller
+		ctrl.Move( transform.forward * Time.deltaTime * vertical * 5f ); // move along forward facing
+		ctrl.transform.Rotate( 0f, horizontal * Time.deltaTime * 90f, 0f );
+
+		// 3. let's add gravity
+		ctrl.Move( Physics.gravity * 0.05f ); // move the controller down
+
+		// 4. let us press SPACE to jump up?
+		if ( Input.GetKeyDown(KeyCode.Space) && ctrl.isGrounded==true ) {
+			ctrl.Move( new Vector3(0f, 6f, 0f) );
+			// 5. code acceleration and velocity???? make gravity / jump force more gradual
+			// we'll do this on Thursday
+		}
+
+	}
+}
---
+++ week02_vectorinput/Assets/scripts/TransformMoveDemo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMoveDemo : MonoBehaviour {
+	
+	void Update () {
+		// hold W to move forward along global Z axis
+		if ( Input.GetKey( KeyCode.W ) ) {
+			// this moves us along the global Z axis
+			// transform.position += new Vector3( 0f, 0f, 1f );
+
+			// 3 PERFECTLY OK ways of moving "forward", relative to the way the cube is facing
+			// transform.Translate( 0f, 0f, 1f * Time.deltaTime); // Translate always accounts for rotation
+			// transform.position += transform.forward * 1f; // transform.forward = current forward facing
+			transform.position += transform.TransformVector( new Vector3(0f, 0f, 1f) );
+		}
+
+		// hold A to turn left
+		if ( Input.GetKey(KeyCode.A) ) {
+			// remember, use euler angles for 0-360 degrees, NOT transform.rotation
+			transform.localEulerAngles += new Vector3( 0f, -90f * Time.deltaTime, 0f );
+		}
+		// hold D to turn right
+		if ( Input.GetKey(KeyCode.D) ) {
+			transform.localEulerAngles += new Vector3( 0f, 90f * Time.deltaTime, 0f );
+		}
+
+	}
+}
