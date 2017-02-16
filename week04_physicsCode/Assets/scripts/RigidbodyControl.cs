edc556d1fed3b13a819448dce0b02077d0c34705
Robert Yang <yang.robert.w@gmail.com>
2017-02-16 16:51:48 -0500

    week04: added physics Rigidbody code / Trigger demo code

---
+++ week04_physicsCode/Assets/scripts/RigidbodyControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyControl : MonoBehaviour {
+
+	Rigidbody rbody; // shortcut for accessing rigidbody
+	Vector3 inputVector; // var we use to pass info from Update > FixedUpdate
+	bool isGrounded; // result of the raycast below the player
+
+	public float maxSpeed = 5f;
+
+	void Start () {
+		rbody = GetComponent<Rigidbody>(); // cache reference for our shortcut
+	}
+
+	// get player input in Update()
+	void Update () {
+		// MOVEMENT
+		float horizontal = Input.GetAxis("Horizontal"); // Left/Right, A/D
+		float vertical = Input.GetAxis("Vertical"); // Up/Down, W/S
+		inputVector = new Vector3( horizontal, 0f, vertical ); 
+		// TURNING: we don't do this in physics usually
+		transform.Rotate( 0f, Input.GetAxis("Mouse X") * Time.deltaTime * 180f, 0f );
+		// JUMPING: but jump only if grounded
+		if ( Input.GetKeyDown(KeyCode.Space) && isGrounded ) {
+			inputVector.y = 50f;
+		}
+	}
+
+	// this is the framerate where PHYSICS runs
+	void FixedUpdate () {
+		// apply Y movement force
+		rbody.AddRelativeForce( Vector3.up * inputVector.y * 10f );
+		inputVector.y = 0f;
+		// apply X/Z movement force
+		if ( rbody.velocity.magnitude < maxSpeed ) {
+			rbody.AddRelativeForce( inputVector * 10f ); // actually move object now
+		}
+
+		// DO GROUNDED CHECK: shoot raycast just a little past bottom of capsule
+		isGrounded = Physics.Raycast( transform.position, Vector3.down, 1.1f );
+	}
+}
---
+++ week04_physicsCode/Assets/scripts/TriggerDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDemo : MonoBehaviour {
+
+	// "other" will be different based on where you put the script
+	// if on the trigger, then "other" is the thing entering the trigger
+	// if on the activator, then "other" is the trigger it entered
+	void OnTriggerEnter ( Collider other ) {
+		// Destroy( activator ); // this will just delete the Collider
+		// Destroy( other.gameObject, 1f ); // this will delete the whole thing after waiting 1 second
+	}
+
+	// "update" for triggers, fires every frame as long as the colliders are intersecting
+	void OnTriggerStay ( Collider other ) {
+		other.transform.localScale -= Vector3.one * Time.deltaTime; // shrink over 1 second
+	}
+
+	// fires when the collision pair is no longer intersecting
+	void OnTriggerExit ( Collider other ) {
+		Debug.Log("object left the trigger!");
+	}
+
+}
